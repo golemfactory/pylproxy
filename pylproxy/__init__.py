7d7c784df9a7784d92d3cfa3d5f59d2b5376f3d6
scx1332 <sieciech.czajka@golem.network>
2024-04-17 23:21:45 +0200

    chore: setup project and linters

--- pylproxy/__init__.py
+++ pylproxy/__init__.py
@@ -1,6 +1,46 @@
+import os
+import asyncio
+import time
+import aiohttp
+from aiohttp import web
+import socket
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    pid = os.getpid()
+    text = "{:.2f}: Hello {}! Process {} is treating you\n".format(time.time(), name, pid)
+
+    # parse X-Server-Addr to get the server address
+    server_addr = request.headers.get('X-Server-Addr', None)
+    # parse X-Server-Port to get the server port
+    server_port = request.headers.get('X-Server-Port', None)
+    # parse X-Remote-Addr to get the remote address
+    remote_addr = request.headers.get('X-Remote-Addr', None)
+    logger.info(f"Server address: {server_addr}")
+    logger.info(f"Server port: {server_port}")
+    logger.info(f"Remote address: {remote_addr}")
+
+    return web.Response(text=text)
+
+
 class PylProxy:
     def __init__(self):
         pass
 
     def __repr__(self):
         return "PylProxy()"
+
+    async def start(self, host, port):
+        app = web.Application()
+        app.router.add_route('GET', '/{tail:.*}', handle)
+
+        runner = aiohttp.web.AppRunner(app)
+        await runner.setup()
+        self._site = aiohttp.web.TCPSite(runner, host, port)
+        await self._site.start()
+
+        logger.info(f"Server started at http://{host}:{port}")
--- tests/test_pyl.py
+++ tests/test_pyl.py
@@ -1,4 +1,6 @@
+import asyncio
 import logging
+import pylproxy
 
 pytest_plugins = ("pytest_asyncio",)
 
@@ -6,9 +8,13 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-async def test_pylproxyimport():
+async def test_pyl():
     import pylproxy
 
     prox = pylproxy.PylProxy()
-    logger.info(prox)
-    assert prox
+    proxy = await prox.start("0.0.0.0", 9990)
+    await asyncio.sleep(3600)
+
+
+if __name__ == "__main__":
+    asyncio.run(test_pyl())
