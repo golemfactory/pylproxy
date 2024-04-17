4e882ee91e785ccf902fb86c4254dfacfa2e6b81
scx1332 <sieciech.czajka@golem.network>
2024-04-17 16:33:13 +0200

    chore: setup project and module

---
+++ tests/test_pyl.py
@@ -0,0 +1,16 @@
+import asyncio
+import pytest
+import logging
+
+pytest_plugins = ('pytest_asyncio',)
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+async def test_pylproxyimport():
+    import pylproxy
+
+    prox = pylproxy.PylProxy()
+    logger.info(prox)
+    assert prox
